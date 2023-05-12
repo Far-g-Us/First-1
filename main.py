238d229d86a1c7735e8dc70322518f27bdc83cef
MKA <MKA@ULTOP.local>
2023-05-12 21:20:40 +0400

    Reload main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# type_int = 1 #целые числа
+# type_float = 1,23 #числа с точкой
+# type_str = "Строки пишутся в кавычках" 
+# type_bool_one = True #булево значение правда, истина 
+# type_bool_two = False #булевы значение ложь
+# type_none = None
