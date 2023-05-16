7a123b7f47b9ff17864bb52dde5e7e0686217c59
MKA <MKA@ULTOP.local>
2023-05-16 21:05:39 +0400

    Upload main.py

--- main.py
+++ main.py
@@ -43,14 +43,54 @@
 
 #--------------------------------------------------------------#
 
-print("Заполните информацию")
+# print("Заполните информацию")
 
-myName = input("Введите имя: ")
-myFamily = input("Введите фамилию: ")
-myAge = int(input("Введите ваш возраст: "))
-myCountry = input("Введите страну: ")
+# myName = input("Введите имя: ")
+# myFamily = input("Введите фамилию: ")
+# myAge = int(input("Введите ваш возраст: "))
+# myCountry = input("Введите страну: ")
 
-print("Имя:", myName)
-print("Фамилия:", myFamily)
-print("Возраст:", myAge)
-print("Страна:", myCountry)
+# print("Имя:", myName)
+# print("Фамилия:", myFamily)
+# print("Возраст:", myAge)
+# print("Страна:", myCountry)
+
+#----------------------------------------------------------------#
+
+# myName = "Ден"
+# print("Привет"+ " " + myName)
+# print(f"Привет {myName}")
+# print("Привет"," ","Ден")
+
+# z = int(input("Введите число: "))
+# b = z//10
+# c = z%10
+# print(b)
+# print(c)
+
+# z =int(input("Введите число: "))
+# x1 = z//100
+# x2 = (z//10)%10
+# x3 = z%10
+# print(x1)
+# print(x2)
+# print(x3)
+# c = x1+x2+x3
+# print(c)
+
+# x =int(input("Введите число: "))
+# x1 = x%10
+# print(x1)
+# x = 123
+# x2 = x%10
+# print(x2)
+# x = 12
+# x3 = x%10
+# print(x3)
+# x = 1
+# x4 = x%10
+# print(x4)
+
+z =str(input("Введите 1 число: "))
+x =str(input("Введите 2 число: "))
+print(z + x)
