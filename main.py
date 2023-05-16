62fe866713ce6b86f148bf507c83b4d34f7d2553
MKA <MKA@ULTOP.local>
2023-05-16 19:34:49 +0400

    Upload main.py

--- main.py
+++ main.py
@@ -4,3 +4,54 @@
 # type_bool_one = True #булево значение правда, истина 
 # type_bool_two = False #булевы значение ложь
 # type_none = None
+
+# a = 123
+# print(a *2, type(a))
+# a = str(a)
+# print(a *2, type(a))
+# a = float(a)
+# print(a *2, type(a))
+
+# b = "123"
+# print(b, type(b))
+# b = int(b)
+# print(b, type(b))
+
+# c = int(input("Введите число - мы умножим его на 2: ")) #input() по умолчанию является строкой
+# c = int(c) #из строки в число
+# print(c *2, type(c))
+
+
+
+#--------------------------------------------------------------#
+
+print("Заполните информацию")
+
+myName = input("Введите имя: ")
+print("Имя:", myName)
+
+myFamily = input("Введите фамилию: ")
+print("Фамилия:", myFamily)
+
+myAge = input("Введите ваш возраст: ")
+myAge = int(myAge)
+print("Возраст:", myAge)
+
+myCountry = input("Ваша страна: ")
+print("Страна:", myCountry)
+
+
+#--------------------------------------------------------------#
+
+# print("Заполните информацию")
+
+# myName = input("Введите имя: ")
+# myFamily = input("Введите фамилию: ")
+# myAge = input("Введите ваш возраст: ")
+# myAge = int(myAge)
+# myCountry = input("Ваша страна: ")
+
+# print("Имя:", myName)
+# print("Фамилия:", myFamily)
+# print("Возраст:", myAge)
+# print("Страна:", myCountry)
