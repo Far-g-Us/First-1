1bdc89a585c4054f528d5399eb668906688770db
MKA <MKA@ULTOP.local>
2023-06-29 21:10:24 +0400

    Upload

--- TOP/main.py
+++ TOP/main.py
@@ -1,5 +1,8 @@
 # Делегирование + мышление ООП
 
+
+
+
 # двигатель и его функции
 # def start():
 #     print("Запуск")
@@ -57,58 +60,179 @@
 # двигатель
 
 # auto = Car("audi","green")
-import copy
-# создание персонажа
-def attack():
-    print("Включен режим атаки")
-def shot():
-    print("выстрел")
-Person = {
-    "name" : "Варвар",
-    "gender" : "Мужской",
-    # "actions" : {
-    #     "attack" : attack
-    # }
-}
+# import copy
+# # создание персонажа
+# def attack():
+#     print("Включен режим атаки")
+# def shot():
+#     print("выстрел")
+# Person = {
+#     "name" : "Варвар",
+#     "gender" : "Мужской",
+#     # "actions" : {
+#     #     "attack" : attack
+#     # }
+# }
 
 
-# создание расы на основе персонажа
-Human = copy.deepcopy(Person)
-Human["race"] = "Человек"
-Human["skills"] = "Быстрый бег", "Красноречие"
+# # создание расы на основе персонажа
+# Human = copy.deepcopy(Person)
+# Human["race"] = "Человек"
+# Human["skills"] = "Быстрый бег", "Красноречие"
 
-Orc = copy.deepcopy(Person)
-Orc["race"] = "Орк"
-Orc["skills"] = "Выносливость", "Зог-зог"
-
-
-# создание ролей на основе рас
-Warrior = copy.deepcopy(Human) or copy.deepcopy(Orc)
-Warrior["role"] = "Воин"
-Warrior["desk"] = "Воин - это..."
-# Warrior["actions"]["attack"]("удар")
-
-Archer = copy.deepcopy(Human) 
-Archer["role"] = "Лучник"
-Archer["desk"] = "Лучник - это..."
-# Archer["actions"]["attack"]("стрельба")
-
-Shaman = copy.deepcopy(Orc)
-Shaman["role"] = "Шаман"
-Shaman["desk"] = "Шаман - это..."
-# Shaman["actions"]["attack"]("заклинание")
+# Orc = copy.deepcopy(Person)
+# Orc["race"] = "Орк"
+# Orc["skills"] = "Выносливость", "Зог-зог"
 
 
-print("-------------------------------------------------------")
-print(Person)
-print("-------------------------------------------------------")
-print(Human)
-print("-------------------------------------------------------")
-print(Orc)
-print("-------------------------------------------------------")
-print(Warrior)
-print("-------------------------------------------------------")
-print(Archer)
-print("-------------------------------------------------------")
-print(Shaman)
-print("-------------------------------------------------------")
+# # создание ролей на основе рас
+# Warrior = copy.deepcopy(Human) or copy.deepcopy(Orc)
+# Warrior["role"] = "Воин"
+# Warrior["desk"] = "Воин - это..."
+# # Warrior["actions"]["attack"]("удар")
+
+# Archer = copy.deepcopy(Human) 
+# Archer["role"] = "Лучник"
+# Archer["desk"] = "Лучник - это..."
+# # Archer["actions"]["attack"]("стрельба")
+
+# Shaman = copy.deepcopy(Orc)
+# Shaman["role"] = "Шаман"
+# Shaman["desk"] = "Шаман - это..."
+# # Shaman["actions"]["attack"]("заклинание")
+
+
+# print("-------------------------------------------------------")
+# print(Person)
+# print("-------------------------------------------------------")
+# print(Human)
+# print("-------------------------------------------------------")
+# print(Orc)
+# print("-------------------------------------------------------")
+# print(Warrior)
+# print("-------------------------------------------------------")
+# print(Archer)
+# print("-------------------------------------------------------")
+# print(Shaman)
+# print("-------------------------------------------------------")
+
+
+
+# Классы в Python
+
+
+
+class Car:
+    def __init__(self,color,marka,motor,wheel,headlights): # создание переменных для класса (объект)
+        self.color = color
+        self.marka = marka
+        self.engine = motor
+        self.wheel = wheel
+        self.headlights = headlights
+
+    # методы действия с определёнными классами
+    def showColor(self):
+        print(self.color)
+
+    def showMarka(self):
+        print(self.marka)
+    
+class Engine:
+    def __init__(self,HP,volume):
+        self.HP = HP
+        self.volume = volume
+
+    def start(self):
+        print("Запуск")
+
+    def stop(self):
+        print("Стоп")    
+
+class Wheel:
+    def __init__(self,turningRadius,diameterWheel):
+        self.turningRadius = turningRadius
+        self.diameterWheel = diameterWheel
+
+    def turnLeft(self):
+        print("Поворот влево")
+
+    def turnRight(self):
+        print("Поворот право")
+
+class Headlights:
+    def __init__(self,optics):
+        self.optics = optics
+    def on(self):
+        print("Вкл фар")
+
+    def off(self):
+        print("Выкл фар")
+
+myHeadlights = Headlights("LED")
+myWheel = Wheel("1960","18")
+myEngine = Engine("120","2.0")
+twoEngine = Engine("280","2.2")
+myAuto = Car("green","audi",twoEngine,myWheel,myHeadlights)
+
+class Abs:
+    def __init__(self,availability):
+        self.availability = availability
+
+# print(myAuto.engine.HP)
+
+# myEngine.start()
+
+
+
+# наследование
+
+class SportCar(Car):
+    def __init__(self, color, marka, motor, wheel, headlights, abs):
+        # self.color = color
+        # self.marka = marka
+        # self.engine = motor
+        # self.wheel = wheel
+        # self.headlights = headlights
+        self.abs = abs
+        super().__init__(color, marka, motor, wheel, headlights)
+
+class SportEngine(Engine):
+    def __init__(self, HP, volume,turbo):
+        self.turbo = turbo
+        super().__init__(HP, volume)
+
+myHeadlights = Headlights("Halogen")
+myWheel = Wheel("1820","19")
+myAbs = Abs("Yes")
+porsheEngine = SportEngine("540","6.0",2)
+twoAuto = SportCar("blue","porshe",porsheEngine,myWheel,myHeadlights,myAbs)
+
+
+
+
+
+
+class Animal:
+    def __init__(self,name,sound):
+        self.name = name
+        self.sound = sound
+    
+    def activeSound(self):
+        print(self.sound)
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Мяу")
+
+    def porr(self):
+        print("Мурлыкает")
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Гав")
+
+    def digHole(self):
+        print("Копает яму")
+
+myCat = Cat("Вася")
+myCat.porr()
