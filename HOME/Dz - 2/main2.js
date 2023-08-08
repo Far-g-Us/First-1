447c0f135ecf7b00bce57e60d3cd283bc2b3bf72
MKA <MKA@ULTOP.local>
2023-08-08 21:11:15 +0400

    Update

--- HOME/Dz - 2/main2.js
+++ HOME/Dz - 2/main2.js
@@ -2,23 +2,24 @@
 x = +prompt("Введите число: ")
 y = +prompt("Введите число: ")
 z = +prompt("Введите число: ")
-m = [x,y,z]
+// m = [x,y,z]
 
 numbers = +prompt("Выберите: \n1 - максимум из трёх \n2 - минимум из трёх \n3 - среднеарифметическое из трёх")
 if (numbers == 1) {
-    b = max(m)
+    b = Math.max(x,y,z)
     console.log(b)
 } else if (numbers == 2) {
-    b = min(m)
+    b = Math.min(x,y,z)
     console.log(b)
 } else if (numbers == 3) {
-    if (y < x < z || z < x < y) {
-        console.log(x)
-    } else if (x < y < z || z < y < x) {
-        console.log(y)
-    } else {
-        console.log(z)
-    }
+    // if (y < x < z || z < x < y) {
+    //     console.log(x)
+    // } else if (x < y < z || z < y < x) {
+    //     console.log(y)
+    // } else {
+    //     console.log(z)
+    // }
+    console.log((x+y+z)/3)
 } else {
     console.log("Вы не выбрали ни один из вариантов")
 }
