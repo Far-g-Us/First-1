583482179f261ba9636966568276f970600228f6
MKA <MKA@ULTOP.local>
2023-09-12 21:11:46 +0400

    Update

--- TOP/django/pythonProject1/block/models.py
+++ TOP/django/pythonProject1/block/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=50)                      # Строка с огранич знаками
-    desc = models.CharField(max_length=150)                      # Строка с огранич знаками
-    image = models.ImageField(upload_to='block/image/')          # Строка под изображение
-    date = models.DateField()                                    # Data
-    url = models.URLField()                                      # Ссылка
+    title = models.CharField('Заголовок', max_length=50)                         # Строка с огранич знаками
+    desc = models.TextField('Описание')                                          # Строка с огранич знаками
+    image = models.ImageField('Изображение', upload_to='block/image/')           # Строка под изображение
+    date = models.DateField('Дата')                                              # Data
+    url = models.URLField('Доп. источник', blank=False)                          # Ссылка
+
+    def __str__(self):
+        return f"{self.title} | {self.date}"
