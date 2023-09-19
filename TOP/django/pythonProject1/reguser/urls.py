6f09b3aaa6959c7f07eed409316c70df257fc4c7
MKA <MKA@ULTOP.local>
2023-09-19 21:31:34 +0400

    Update

--- TOP/django/pythonProject1/reguser/urls.py
+++ TOP/django/pythonProject1/reguser/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('register/', reguserView, name='reguser'),
+    path('login/', loginView, name='login'),
+    path('logout/', logoutView, name='logout'),
+    path('profile/', profileView, name='profile'),
+    path('profileup/', profileupView, name='profileup'),
 ]
--- TOP/django/pythonProject1/reguser/views.py
+++ TOP/django/pythonProject1/reguser/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.db import IntegrityError
+
+
+from .forms import UserProfileForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def reguserView(request):
     if request.method == 'GET':
@@ -21,4 +26,35 @@ def reguserView(request):
 
 
 def loginView(request):
-    return render(request, template_name='./login/login.html')
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        return render(request, './login/login.html', {'form': form})
+    else:
+        form = AuthenticationForm(request.POST)
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        try:
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        except:
+            return render(request, './login/login.html', {'form': form, 'error': 'Неверный логин или пароль'})
+
+def logoutView(request):
+    logout(request)
+    return redirect('home')
+
+def profileView(request):
+    user_profile = request.user.userprofile
+    return render(request, './profile/profile.html', {'user_profile': user_profile})
+
+def profileupView(request):
+    user_profile = request.user.userprofile
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профиль успешно обновлён')
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=user_profile)
+        return render(request, 'profile/profileup.html', {'form': form})
